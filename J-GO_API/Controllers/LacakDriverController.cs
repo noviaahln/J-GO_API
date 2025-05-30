3f8d17ed74c2da7c5210db3dc90aa7dd860e901c
noviaahln <161816037+noviaahln@users.noreply.github.com>
2025-05-30 20:54:09 +0700

    BE fitur lacak driver newAdd project files.

---
+++ J-GO_API/Controllers/LacakDriverController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using J_GO_API.Models;
+
+namespace J_GO_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LacakDriverController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public LacakDriverController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Lokasi>> GetAll()
+        {
+            string connString = _configuration.GetConnectionString("koneksi");
+            LokasiContext context = new LokasiContext(connString);
+            return Ok(context.ListLokasi());
+        }
+
+        [HttpPost]
+        public ActionResult Create([FromBody] Lokasi loc)
+        {
+            string connString = _configuration.GetConnectionString("koneksi");
+            LokasiContext context = new LokasiContext(connString);
+
+            bool result = context.CreateLokasi(loc);
+
+            if (result)
+                return Ok(new { message = "Data lokasi berhasil ditambahkan." });
+            else
+                return BadRequest(new { message = "Gagal menambahkan data lokasi." });
+        }
+    }
+}
---
+++ J-GO_API/Models/LokasiContext.cs
@@ -0,0 +1,58 @@
+﻿using J_GO_API.Helpers;
+
+namespace J_GO_API.Models
+{
+    public class LokasiContext
+    {
+        private readonly SqlDBHelper _dbHelper;
+
+        public LokasiContext(string connectionString)
+        {
+            _dbHelper = new SqlDBHelper(connectionString);
+        }
+
+        public List<Lokasi> ListLokasi()
+        {
+            var result = new List<Lokasi>();
+            string query = "SELECT * FROM \"Lokasi\"";
+
+            var data = _dbHelper.ExecuteReader(query);
+            foreach (var row in data)
+            {
+                result.Add(new Lokasi
+                {
+                    id_lokasi = Convert.ToInt32(row["id_lokasi"]),
+                    id_driver = Convert.ToInt32(row["id_driver"]),
+                    waktu_update = (TimeSpan)row["waktu_update"],
+                    longitude = Convert.ToDouble(row["longitude"]),
+                    latitude = Convert.ToDouble(row["latitude"])
+                });
+            }
+
+            return result;
+        }
+
+        public bool CreateLokasi(Lokasi loc)
+        {
+            string query = @"INSERT INTO ""Lokasi"" (id_driver, waktu_update, longitude, latitude) 
+                             VALUES (@id_driver, @waktu_update, @longitude, @latitude)";
+            var parameters = new Dictionary<string, object>
+            {
+                { "@id_driver", loc.id_driver },
+                { "@waktu_update", loc.waktu_update },
+                { "@longitude", loc.longitude },
+                { "@latitude", loc.latitude }
+            };
+
+            try
+            {
+                _dbHelper.ExecuteNonQuery(query, parameters);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
